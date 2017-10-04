caea14c11fc61685f80f266f426f9639ae562bf0
Praveen Garg1 <praveen.garg1@guavus.com>
2017-10-04 12:06:02 +0530

    some changes in xml for handle  d3 change.

--- classes/Pages/MRXScreens/SegmentScreenClass.py
+++ classes/Pages/MRXScreens/SegmentScreenClass.py
@@ -3,6 +3,7 @@ from classes.Components.MulitpleDropdownComponentClass import *
 from classes.Components.DropdownComponentClass import *
 from classes.Components.TableComponentClass import *
 from classes.Pages.GlobalFiltersPopClass import *
+from classes.Pages.ExplorePageClass import *
 
 class SegmentScreenClass(BasePageClass):
     def __init__(self,driver):
